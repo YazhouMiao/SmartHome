f5edb61e833af26d4cb7d1b543f2b1a42a8011e0
myzhou1991 <ibaiwei@yeah.net>
2017-02-18 17:54:22 +0800

    测试

--- robot/test.js
+++ robot/test.js
@@ -1,13 +1,20 @@
 var five = require('johnny-five');
+var Raspi = require('raspi-io');
 
-var pin = new five.Pin(7);
+var board = new five.Board({
+    io: new Raspi()
+});
 
-var val = setInterval(function(){
-    pin.query(function(status){
-        pin.write(!status);
-    });
-},2000);
+board.on('ready',function(){
+    var pin = new five.Pin(7);
 
-setTimeout(function(){
-    clearInterval(val);
-},10000);
+    var val = setInterval(function(){
+        pin.query(function(status){
+            pin.write(!status);
+        });
+    },2000);
+
+    setTimeout(function(){
+        clearInterval(val);
+    },10000);
+});
