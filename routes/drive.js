6b84ae1a9aedb80a0406513d2afd8029d4f7fb67
myzhou1991 <ibaiwei@yeah.net>
2017-03-01 23:30:19 +0800

    手控机器人行进

--- routes/drive.js
+++ routes/drive.js
@@ -1,6 +1,6 @@
 /**
  * Created by myzhou on 2017/3/1.
- * 人为控制智能机器人行进
+ * 手动控制智能机器人行进
  */
 var router = require('express').Router();
 var Move = require('../robot/move');
@@ -11,34 +11,41 @@ router.get('/',function(req,res,next){
 });
 
 router.post('/',function(req,res,next){
-    switch(req.param('opt')){
+    switch(req.body.opt){
         case 'forward':
             move.forward();
             res.json({errcode:0});
+            break;
 
         case 'back':
             move.back();
             res.json({errcode:0});
+            break;
 
         case 'stop':
             move.stop();
             res.json({errcode:0});
+            break;
 
         case 'front_left':
             move.turn('front_left');
             res.json({errcode:0});
+            break;
 
         case 'front_right':
             move.turn('front_right');
             res.json({errcode:0});
+            break;
 
         case 'back_left':
             move.turn('back_left');
             res.json({errcode:0});
+            break;
 
         case 'back_right':
             move.turn('back_right');
             res.json({errcode:0});
+            break;
 
         default:
             res.json({errcode:1});
