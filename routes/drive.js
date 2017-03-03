5737161c9fc056e30490f9edbcea1c366fbee30c
myzhou1991 <ibaiwei@yeah.net>
2017-03-03 21:05:42 +0800

    调试

--- routes/drive.js
+++ routes/drive.js
@@ -3,8 +3,8 @@
  * 手动控制智能机器人行进
  */
 var router = require('express').Router();
-// var Move = require('../robot/move');
-// var move = new Move();
+var Move = require('../robot/move');
+var move = new Move();
 
 router.get('/',function(req,res,next){
     res.render('drive');
@@ -13,37 +13,37 @@ router.get('/',function(req,res,next){
 router.post('/',function(req,res,next){
     switch(req.body.opt){
         case 'forward':
-            // move.forward();
+            move.forward();
             res.json({errcode:0});
             break;
 
         case 'back':
-            // move.back();
+            move.back();
             res.json({errcode:0});
             break;
 
         case 'stop':
-            // move.stop();
+            move.stop();
             res.json({errcode:0});
             break;
 
         case 'front_left':
-            // move.turn('front_left');
+            move.turn('front_left');
             res.json({errcode:0});
             break;
 
         case 'front_right':
-            // move.turn('front_right');
+            move.turn('front_right');
             res.json({errcode:0});
             break;
 
         case 'back_left':
-            // move.turn('back_left');
+            move.turn('back_left');
             res.json({errcode:0});
             break;
 
         case 'back_right':
-            // move.turn('back_right');
+            move.turn('back_right');
             res.json({errcode:0});
             break;
 
